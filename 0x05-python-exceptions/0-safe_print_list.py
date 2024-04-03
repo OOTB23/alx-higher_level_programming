652032b52d0c686058be180fd8434e67aa8a98d8
OKOTB <osamaquotp@gmail.com>
2024-04-02 19:52:02 -0700

    fix code

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3def safe_print_list(my_list=[], x=0):
+def safe_print_list(my_list=[], x=0):
     try:
         key = 0
         while key < x :  
             print(my_list[key] , end ="")
             key = key +1
+        print("")
         return key
     except IndexError:
+        print("")
         return key
-    
-new_list = safe_print_list([1,2,3,4,5],5)
-print ("\n",new_list,sep="")
-
